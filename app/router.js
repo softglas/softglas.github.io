39ed920bb8d688e19669708957206e2b665b898a
Carlos Solares <csolares23@gmail.com>
2015-08-30 21:23:02 -0700

    added google analytics

--- app/router.js
+++ app/router.js
@@ -8,4 +8,16 @@ var Router = Ember.Router.extend({
 Router.map(function() {
 });
 
+
+
+// Re open config
+Router.reopen({
+  notifyGoogleAnalytics: function() {
+    return ga('send', 'pageview', {
+        'page': this.get('url'),
+        'title': this.get('url')
+      });
+  }.on('didTransition')
+});
+
 export default Router;
